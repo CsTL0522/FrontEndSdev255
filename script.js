c0b88f44bc65b327f564cff35096aa04be73458a
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-02-09 02:36:51 -0600

    frontend start

---
+++ script.js
@@ -0,0 +1,12 @@
+// event listener triggered when DOM loaded, visit
+addEventListener("DOMContentLoaded", async function() {
+    const response = await this.fetch("http://localhost:3000/api/songs")
+    const songs = await response.json()
+
+    let html = ""
+    for (let song of songs){
+        html+= `<li> ${song.title} - ${song.artist}</li>`
+    }
+
+    document.querySelector("#addedsong").innerHTML = html
+})
