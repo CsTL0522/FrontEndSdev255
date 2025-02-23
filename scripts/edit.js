c1d0f869bb58452646294cfe1ed5fa91de858454
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-02-23 06:23:45 -0600

    fixed functionality

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,14 +1,14 @@
 addEventListener("DOMContentLoaded", async function(){
-    document.querySelector("updateBtn").addEventListener("click", updateSong)
+    document.querySelector("#updateBtn").addEventListener("click", updateSong)
     const urlparam =new URLSearchParams(window.location.search)
     const songID = urlparam.get('id')
-    const response = await this.fetch("http://localhost:3000/api/songs/" +songID)
+    const response = await fetch("http://localhost:3000/api/songs/" +songID)
     if(response.ok){
         let song = await response.json()
         document.querySelector("#songId").value = song._id
         document.querySelector("#title").value = song.title
         document.querySelector("#artist").value = song.artist
-        document.querySelector("#releasede").value = song.releaseDate.substring(0,10)
+        document.querySelector("#released").value = song.releaseDate.substring(0,10)
         document.querySelector("#popularity").value = song.popularity
         document.querySelector("#genre").value = song.genre
     }
